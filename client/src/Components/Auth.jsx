8d713155656a88ee5771904a3b522092b354d221
River <stein.jessicarose@gmail.com>
2022-09-26 00:12:44 -0600

    small changes saved

--- client/src/Components/Auth.jsx
+++ client/src/Components/Auth.jsx
@@ -5,12 +5,27 @@ import axios from 'axios';
 import signInImage from '../assets/signup.jpg';
 
 const Auth = () => {
-    const [isSignUp, setIsSignUp] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(true);
+    const handleChange = () => {}
     return (
         <div className='auth__form-container'>
             <div className='auth__form-container_fields'>
                 <div className='auth__form-container_fields-content'>
                     <p>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit= {() => {}}>
+                        {isSignUp && (
+                            <div className='auth__form-container_fields-content_input'>
+                                <label htmlFor='fullName'>Full Name</label>
+                                <input
+                                    name='fullName'
+                                    type='text'
+                                    placeholder='Full Name'
+                                    onChange={handleChange}
+                                    required
+                                />    
+                            </div>
+                        )}
+                    </form>
                 </div>
             </div>
         </div>
