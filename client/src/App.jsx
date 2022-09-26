ebefee6a8b74f79f7676e1f786128b11790493da
River <stein.jessicarose@gmail.com>
2022-09-26 00:00:41 -0600

    sign in page started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,25 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import { Cookies } from 'universal-cookie';
 
-import {ChannelContainer, ChannelListContainer, ChannelSearch, TeamChannelList} from './Components';
+import { ChannelContainer, ChannelListContainer, Auth } from './Components';
 import './App.css';
+
+const authToken = false;
 
 const apikey = '2tepc2m8nkat';
 
 const client = StreamChat.getInstance(apikey);
 
 const App = () => {
-  return (
-    <div className='app__wrapper'>
-        <Chat client = {client} theme="team light">
-            <ChannelListContainer/>
-            <ChannelContainer/>
-        </Chat>
-    </div>
-  )
+    if(!authToken){ return <Auth/>}
+    else{ return (
+        <div className='app__wrapper'>
+            <Chat client = {client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
+        </div>
+    )}
 }
 
 export default App
